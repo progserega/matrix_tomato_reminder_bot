628bd210c2a53151907ff58e6907cd1440d5bda0
progserega <progserega@gmail.com>
2026-08-25 16:01:04 +1000

    поправил опции, прокинул конфиг на хост

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,9 @@ set -e
 
 echo "Генерация config.ini из переменных окружения..."
 
-cat <<EOF > /app/config.ini
+mkdir /app/config
+
+cat <<EOF > /app/config/config.ini
 [matrix]
 matrix_server = ${MATRIX_SERVER:-https://matrix.org}
 matrix_login = ${MATRIX_LOGIN}
@@ -15,6 +17,26 @@ session_store_path = /var/spool/matrix_bot/session.json
 [storage]
 data_file = /var/spool/matrix_bot/data.json
 
+[time_presets]
+morning = 7:00
+on_work = 8:00
+lunch_break = 12:00
+after_lunch = 13:00
+after_work = 17:00
+evening = 21:00
+
+[invites]
+# for disable some options - live in empty.
+# invite rules - who can invite this bot (mxid users and its domain) - this
+# options override all other options in this block:
+allow_users = ${ALLOW_USERS}
+# allow whole domain (or all domains: *):
+allow_domains = ${ALLOW_DOMAINS}
+# disable some users. Also from allowed domains:
+deny_users = ${DENY_USERS}
+# can disable some domains if allow was by mask '*':
+deny_domains = ${DENY_DOMAINS}
+
 [logging]
 log_path = /var/log/matrix_bot/matrix_tomato_reminder_bot.log
 debug = ${DEBUG_MODE:-no}
@@ -23,4 +45,4 @@ log_backup_when = midnight
 EOF
 
 echo "Конфигурация создана. Запуск бота..."
-exec python bot.py /app/config.ini
+exec python bot.py /app/config/config.ini
